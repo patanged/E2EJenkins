9f7af2d3dcc549e65ad2befb740506cfb7aea390
DELL <DELL@DESKTOP-9VERA0T>
2022-10-19 19:41:55 +0530

    this is demo project code

---
+++ dinesh/src/dineshdinesh/Dinsh.java
@@ -0,0 +1,18 @@
+package dineshdinesh;
+
+public class Dinsh {
+	
+	
+	public void porint()
+	{
+		System.out.println("dinesh patange");
+	}
+
+	public static void main(String[] args) {
+		
+		Dinsh d = new Dinsh();
+		d.porint();
+	
+	}
+
+}
